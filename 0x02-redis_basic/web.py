a2aeb5858e413392de39531c8c478ed6fdbffb5b
njokujr <njokuvictory216@gmail.com>
2023-07-20 00:36:53 +0000

    advanced task corr

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,28 +6,40 @@ import redis
 from functools import wraps
 from typing import Callable
 
+import time
 
-def track_page_count(method: Callable) -> Callable:
-    """Tracks how many times a particular
-    URL was accessed"""
+# Decorator for caching
+def cache_with_expiry(seconds):
+    cache = {}
 
-    @wraps(method)
-    def wrapper(*args, **kwargs):
-        """Inner Wrapper Function"""
-        cache = redis.Redis()
-        url = args[0]
-        key = 'count:' + url
-        cache.incr(key)
-        page = cache.get(url)
-        if page is not None:
-            return page.decode('utf-8')
-        page = method(*args, **kwargs)
-        cache.setex(key, 10, page)
-        return page
-    return wrapper
+    def decorator(func):
+        def wrapper(url):
+            if url in cache and time.time() - cache[url]['timestamp'] < seconds:
+                print(f"Cache hit for {url}")
+                return cache[url]['content']
+            else:
+                print(f"Cache miss for {url}")
+                content = func(url)
+                cache[url] = {
+                    'content': content,
+                    'timestamp': time.time()
+                }
+                return content
+        return wrapper
+    return decorator
 
+@cache_with_expiry(seconds=10)
+def get_page(url):
+    response = requests.get(url)
+    return response.text
 
-def get_page(url: str) -> str:
-    """Obtain the HTML content of a particular
-    URL and returns it."""
-    return get(url).text
+# Test the function
+url_to_fetch = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://example.com"  # Example slow URL
+page_content = get_page(url_to_fetch)
+print(page_content)
+
+# Test again after 5 seconds to check the caching
+time.sleep(5)
+page_content_cached = get_page(url_to_fetch)
+print(page_content_cached)
+
